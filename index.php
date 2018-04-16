3363e782b31971ed3ee723c5a441797fdf6e5e13
alex <alex.danilenko1988@gmail.com>
2018-04-16 17:31:42 +0300

    var dir

--- index.php
+++ index.php
@@ -4,5 +4,9 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use App\App;
 
+define('APP_ROOT', __DIR__);
+define('APP_CACHE', __DIR__.'/var/cache');
+define('APP_LOGS', __DIR__.'/var/logs');
+
 $app = new App();
 $app->run();
---
+++ src/App/FileKeeper/FileKeeper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\FileKeeper;
+
+/**
+ * Class FileKeeper
+ *
+ * @package App\FileKeeper
+ */
+class FileKeeper
+{
+    /**
+     * FileKeeper constructor.
+     *
+     * @param string $fileUrl
+     */
+    public function __construct(string $fileUrl)
+    {
+
+    }
+}
